8885b2f5134374190d4843ff9bcd9224fa737f76
praveenkothari <praveen.kothari@gmail.com>
2014-02-15 21:23:21 -0600

    create_users

--- scripts/add_yarn_user.sh
+++ scripts/add_yarn_user.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-useradd -s /bin/bash -d /usr/lib/hadoop -g hadoop yarn
+mkdir /home/yarn
+mkdir /home/hive
+mkdir /home/hbase
+mkdir /home/oozie
+mkdir /home/hcat
+mkdir /home/hdfs
+mkdir /home/mapred
+
+useradd -s /bin/bash -d /home/yarn -g hadoop yarn
+useradd -s /bin/bash -d /home/hdfs -g hadoop hdfs
+useradd -s /bin/bash -d /home/mapred -g hadoop mapred
+useradd -s /bin/bash -d /home/hbase -g hadoop hbase
+useradd -s /bin/bash -d /home/hive -g hadoop hive
+useradd -s /bin/bash -d /home/hcat -g hadoop hcat
+useradd -s /bin/bash -d /home/oozie -g hadoop oozie
+useradd -s /bin/bash -d /var/run/zookeeper -g hadoop zookeeper
